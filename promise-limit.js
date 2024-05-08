6f506963e843a6e1e8c5695b14182cc82c9277ac
iisaya <31286515+iisaya255@users.noreply.github.com>
2024-05-09 01:03:19 +0800

    Create promise-limit.js

---
+++ promise-limit.js
@@ -0,0 +1,16 @@
+const aysncPool = aysnc (poolLimit, iter, iterFn) => {
+	const ret = []
+	const exec = new Set()
+
+	for (let item of iter) {
+		const p = Promise.resolve().then(() => iterFn(item, iter))
+		ret.push(p)
+		exec.add(p)
+		const clean = () => exec.delete(p)
+    p.then(clean).catch(clean)
+		if (execute.size >= poolLimit) {
+			await Promise.race(exec)
+		}
+	}
+	return Promise.all(ret)
+}
